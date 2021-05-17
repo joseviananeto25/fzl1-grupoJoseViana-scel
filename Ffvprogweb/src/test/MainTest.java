374932920b31eb87e3e4472c00d56121bbb0bd58
José Neto <jose.viana.neto25@gmail.com>
2021-05-17 15:13:42 -0300

    Commit inicial.

---
+++ Ffvprogweb/src/test/MainTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MainTest {
+	
+	@Test
+	public void name() {
+		assertEquals("Deu certo", "Deu certo");
+	}
+
+}
