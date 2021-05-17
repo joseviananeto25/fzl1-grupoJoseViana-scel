c407f406637c2a49b856a739d99cfc7a3c4b7a5e
José Neto <jose.viana.neto25@gmail.com>
2021-05-17 15:24:05 -0300

    Classe MainTest alterada para cumprir o enunciado:
    1.Considere a aplicação para controle de empréstimo e devolução de
    livros e o cenário de teste “CT01 – cadastrar livro com
    sucesso”, discutido na aula. Faça a implementação utilizando técnicas de
    ATDD e programação por pares.

--- Ffvprogweb/src/test/MainTest.java
+++ Ffvprogweb/src/test/MainTest.java
@@ -1,14 +1,21 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class MainTest {
-	
+
 	@Test
-	public void name() {
-		assertEquals("Deu certo", "Deu certo");
+	void ct01_quando_dados_validos_cadastra_com_sucesso() {
+		// dado que nao existem livros cadastrados
+		Biblioteca biblioteca = new Biblioteca();
+		// quando um livro é cadastrado
+		Livro umLivro = new Livro();
+		umLivro.setAutor("Pressman");
+		umLivro.setIsbn("1111");
+		umLivro.setTitulo("Engenharia de Software");
+		biblioteca.save(umLivro);
+		// entao o total de livros cadastrados igual 1
+		assertEquals(1, biblioteca.size());
 	}
 
 }
