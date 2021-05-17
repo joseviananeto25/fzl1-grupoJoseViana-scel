708903c757cdfe8471e064b7661fcdd2b953e361
José Neto <jose.viana.neto25@gmail.com>
2021-05-17 15:58:20 -0300

    "CT02 – cadastrar livro com isbn já cadastrado"
    Implementação de teste concluída

--- Ffvprogweb/src/model/Livro.java
+++ Ffvprogweb/src/model/Livro.java
@@ -1,14 +1,15 @@
 package model;
 
 public class Livro {
-	
+
 	private String isbn;
 	private String titulo;
 	private String autor;
-	
+
 	public String getIsbn() {
 		return isbn;
 	}
+
 	public String setIsbn(String isbn) {
 		String mensagem = null;
 		if ("".equals(isbn)) {
@@ -16,28 +17,53 @@ public class Livro {
 		} else {
 			this.isbn = isbn;
 		}
-		
+
 		return mensagem;
-		
+
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getAutor() {
 		return autor;
 	}
+
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
-		
+
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (autor == null) {
+			if (other.autor != null)
+				return false;
+		} else if (!autor.equals(other.autor))
+			return false;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
 		return true;
 	}
-	
-	
 
 }
